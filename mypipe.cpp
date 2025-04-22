0dc73a56e56fc4d08133f40c0a3757b4b230f9bb
rahulnbVtech <143011976+rahulnbVtech@users.noreply.github.com>
2025-04-21 22:10:02 -0400

    Add files via upload

--- mypipe.cpp
+++ mypipe.cpp
@@ -5,6 +5,8 @@
 #include <string>
 
 
+// Constructor: Creates a unidirectional pipe using pipe().
+// pipe() initializes two file descriptors: fd[0] for reading, fd[1] for writing.
 mypipe::mypipe()
 {
     auto status{pipe(fd.data())};
@@ -13,12 +15,15 @@ mypipe::mypipe()
     }
 }
 
+// Destructor: Closes both ends of the pipe to release resources
 mypipe::~mypipe()
 {
     close(fd[0]);
     close(fd[1]);
 }
 
+// redirect(): Redirects standard output (STDOUT) to the pipe's write end.
+// This allows capturing output that would normally go to the console.
 void mypipe::redirect()
 {
     dup2(fd[1], STDOUT_FILENO);
@@ -26,6 +31,8 @@ void mypipe::redirect()
     close(fd[1]);
 }
 
+// read(): Reads data from the pipe's read end and returns it as a std::string.
+// Typically used to capture the output that was redirected into the pipe.
 std::string mypipe::read()
 {
     std::array<char, 256> buf = {};
